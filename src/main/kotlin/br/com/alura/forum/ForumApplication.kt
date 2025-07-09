ceea71f25f6de535e34fdbfed2824626522f1e48
Vitor gabriel <vitorgabrieltrabalho@gmail.com>
2025-07-09 13:30:04 -0300

    ajuste e tentativa de push

---
+++ src/main/kotlin/br/com/alura/forum/ForumApplication.kt
@@ -0,0 +1,16 @@
+package br.com.alura.forum
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ForumApplication
+
+fun main(args: Array<String>) {
+	runApplication<ForumApplication>(*args)
+
+
+	println("ola mundo")
+
+
+}
